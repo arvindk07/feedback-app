9ec1fbfd900f79e54a067cc29e986874978db4e7
arvindk07 <info.arvind07@gmail.com>
2023-02-25 10:08:09 +0530

    manage global state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackData from "./data/Feedback";
 
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
-
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
---
+++ src/data/Feedback.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias sunt voluptates quo doloribus unde facilis, magnam consequuntur, dolores nostrum distinctio maiores minus repudiandae itaque laborum atque numquam odit aliquam",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias sunt voluptates quo doloribus unde facilis, magnam consequuntur, dolores nostrum distinctio maiores minus repudiandae itaque laborum atque numquam odit aliquam",
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias sunt voluptates quo doloribus unde facilis, magnam consequuntur, dolores nostrum distinctio maiores minus repudiandae itaque laborum atque numquam odit aliquam",
+  },
+];
+
+export default FeedbackData;
