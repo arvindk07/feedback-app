57f2b7e8720e101c325cd82b0b9d3035937c150d
arvindk07 <info.arvind07@gmail.com>
2023-02-25 09:32:38 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
-      <div className="container"></div>
+
+      <div className="container">
+        <FeedbackItem />
+      </div>
     </>
   );
 };
