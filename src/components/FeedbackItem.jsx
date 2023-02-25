74368ebabbc3dccb0156cd4e40a85dc48718761e
arvindk07 <info.arvind07@gmail.com>
2023-02-25 13:41:55 +0530

    event and prop drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes color="purple" onClick={() => handleDelete(item.id)} />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
--- src/data/Feedback.js
+++ src/data/Feedback.js
@@ -11,7 +11,7 @@ const FeedbackData = [
   },
   {
     id: 3,
-    rating: 9,
+    rating: 8,
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias sunt voluptates quo doloribus unde facilis, magnam consequuntur, dolores nostrum distinctio maiores minus repudiandae itaque laborum atque numquam odit aliquam",
   },
 ];
